f35a58804da5b7bd9dc6778d1d56bbb56c55e9b1
mostafamaged99 <mostafaabdelmaged98@gmail.com>
2024-09-11 00:27:03 +0300

    day 2

--- pythonic/__init__.py
+++ pythonic/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
+from flask_ckeditor import CKEditor
+from flask_modals import Modal
 app = Flask(__name__)
 
 app.config[
@@ -12,8 +14,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pythonic.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+ckeditor = CKEditor(app)
+modal = Modal(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
-
 from pythonic import routes
